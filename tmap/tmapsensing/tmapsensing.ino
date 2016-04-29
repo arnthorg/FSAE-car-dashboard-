c5590be6cf71c73892a9a7cf19bd108cb6204b7a
Arnþór Gíslason <arnthorgisla@gmail.com>
2016-04-29 20:46:03 +0000

    uploading data and new revisions

--- tmap/tmapsensing/tmapsensing.ino
+++ tmap/tmapsensing/tmapsensing.ino
@@ -10,21 +10,29 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-
+unsigned int count = 0;
 char val  = 0;
+bool toggle = 1;
 /*
  * The setup function. We only start the sensors here
  */
 void setup(void)
 {
   // start serial port
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
   Serial.println('a');
   char a = 'b';
   while( a != 'a') {
     a = Serial.read();
+    count++;
+    if(count > 25000) {
+      digitalWrite(13,toggle);
+      toggle = !toggle;
+      count = 0;
+    }
   }
-
+  digitalWrite(13,0);
   // Start up sensor lib
   sensors.begin();
   sensors.setResolution(TEMP_12_BIT);
@@ -43,7 +51,12 @@ void loop(void)
    if (val == 'V') {
     Serial.println(analogRead(A0)*5.0/1024.0);
    }
-  
+      count++;
+    if(count > 2^16) {
+      digitalWrite(13,toggle);
+      toggle = !toggle;
+      count = 0;
+    }
   delay(20);
 }
 
