6e9e035e23b059cfb61ae627a1edb29d755405b5
Arnthorg <arnthorgisla@gmail.com>
2016-05-06 18:33:27 +0000

    added 7seg out

--- rpm/rpm/rpm.ino
+++ rpm/rpm/rpm.ino
@@ -1,18 +1,24 @@
 #define Data 3 // D3
 #define CP 4 //   D4
+#define Data7 5 //D5
+#define CP7 6 
+#define Latch7 7
 const unsigned short MAX_RPM = 9100;
 const byte interruptPin = 2;
 const short BAR_SIZE = 10;
 volatile byte state = LOW;
 unsigned long lastInterrupt;
 unsigned long currentInterrupt;
-//unsigned int deltaT;
 float deltaT;
 unsigned short pos =0;
 double rpm[10] = {0};
 unsigned int rpmAvg;
 double sum;
 bool barGraphArr[BAR_SIZE] = {0};
+bool periodOn7SegDisp = 0;
+byte digit[8] = { 0b00101010,  0b01100000,
+                  0b11011010, 0b11110010, 0b01100110, 
+                  0b10110110, 0b00001010, 0b10011110};
 
 
 
@@ -22,30 +28,34 @@ void setup() {
   pinMode(interruptPin, INPUT_PULLUP);
   pinMode(Data, OUTPUT);
   pinMode(CP, OUTPUT);  
+  pinMode(CP7, OUTPUT); 
+  pinMode(Data7, OUTPUT); 
+  pinMode(Latch7, OUTPUT); 
+  digitalWrite(Latch7, 0);
+  PORTD &= ~((1<<5) | (1<<6) | (1<<7)); // unsetting bits 5-7
   delay(100);
   attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
 }
 
 void loop() {
   calcRpmAvg();
-  Serial.print("rpm: ");
+  /*Serial.print("rpm: ");
   Serial.println(rpmAvg);
   barGraph(rpmAvg);
-  for(int iter = 0; iter < BAR_SIZE; iter++){
-    Serial.print(barGraphArr[BAR_SIZE - iter - 1]); // inverting the output
-    
+  Serial.println("");*/
+  for(int iter = 0;iter<8;iter++) {
+    sevenSeg(iter);
+    //shiftOut(Data7, CP7, MSBFIRST, 1<<iter);
+    delay(500);
   }
-  Serial.println("");
+  periodOn7SegDisp = !periodOn7SegDisp;
   delay(200);
 }
 void barGraph(int rpm) {
   num2array(rpm);
   for(int iter = 0; iter < BAR_SIZE; iter++ ) {
-    /*digitalWrite(Data, barGraphArr[BAR_SIZE - iter - 1]); // inverting the output
-    digitalWrite(CP, 1);
-    digitalWrite(CP, 0);*/
     bool x = barGraphArr[BAR_SIZE - iter - 1];
-    PORTD ^= (-x ^ PORTD) & (1 << 3);
+    PORTD ^= (-x ^ PORTD) & (1 << 3); 
     PORTD |= (1<<4);
     PORTD &= ~(1<<4);
   }
@@ -62,7 +72,6 @@ void blink() {
   if((currentInterrupt - lastInterrupt ) > 5*1000) { //debounce
     calculateRpm();
     lastInterrupt = currentInterrupt;
-    
   }
 }
 void calculateRpm() {
@@ -82,5 +91,16 @@ void num2array(double rpm) {
     }
     else barGraphArr[iter] = 0;
   }
+}
+void sevenSeg(short symbol) {
+  byte digitTmp = digit[symbol];
+  digitTmp ^= (-periodOn7SegDisp ^ digitTmp) & 1;
+  
+  shiftOut(Data7, CP7, LSBFIRST, ~digitTmp);
+  //digitalWrite(Latch7, 1);
+  //digitalWrite(Latch7, 0);
+  PORTD |= (1<<7);
+  delayMicroseconds(5); //datasheet segir 200ns min en þarf alveg 5µs t_w
+  PORTD &= ~(1<<7);
 }
 
