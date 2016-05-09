a2633485bc0ce441f6737135d007e95d05635809
Arnthorg <arnthorgisla@gmail.com>
2016-05-09 15:59:51 +0000

    progress

--- dash/dash.ino
+++ dash/dash.ino
@@ -8,6 +8,7 @@
 #define gearUp 9
 #define gearDown 10
 #define neutral 11 
+#define oilP A0
 
 const unsigned short MAX_RPM = 9100;
 const short BAR_SIZE = 10;
@@ -15,13 +16,13 @@ const byte digit[8] = {  0b00001010, 0b00101010, 0b01100000,        //r,n,1,2,3,
                          0b11011010, 0b11110010, 0b01100110, 
                          0b10110110, 0b10011110};
 
-unsigned long g_lastInterrupt;
-unsigned long g_lastInterrupt2;
-unsigned short g_pos =0;
+volatile unsigned long g_lastInterrupt;
+volatile unsigned long g_lastInterrupt2;
+volatile unsigned short g_pos =0;
 double g_rpm[10] = {0};
 bool g_barGraphArr[BAR_SIZE] = {0};
 bool g_periodOn7SegDisp = 0;
-short g_selectedGear = 32;
+volatile short g_selectedGear = 32;
 
 
 void setup() {
@@ -37,7 +38,7 @@ void setup() {
   pinMode(Latch7, OUTPUT); 
   pinMode(gearUp, INPUT);
   pinMode(gearDown, INPUT);
-  PORTD &= ~((1<<Data7) | (1<<CP7) | (1<<Latch7)); // unsetting bits
+  PORTD &= ~((1<<Data7) | (1<<CP7) | (1<<Latch7)); // unsetting bits, dno if needed
   delay(100); //maybe this delay is important?
   attachInterrupt(digitalPinToInterrupt(interruptPin2), blink, RISING);
   attachInterrupt(digitalPinToInterrupt(interruptPin3), setGear, RISING);
@@ -47,7 +48,10 @@ void setup() {
     sevenSeg(1);
   }
 }
-// todo watchdog timer
+// todo: 
+// watchdog timer
+// skjár
+// lesa frá mælum
 /*static volatile int shared_variable ; // er að reyna að fækka global breytum
 
 int getShared(){ return shared_variable ; }
@@ -60,27 +64,14 @@ void loop() {
   barGraph(calcRpmAvg());
   //sevenSeg(g_selectedGear); // refresh display periodically
   //Serial.println(g_selectedGear);
-  /*if(!neutral) {
-    g_selectedGear =7;
-    sevenSeg(g_selectedGear); //display E on 7seg
-  }
-  else { 
-    g_selectedGear = 1; // 1 being neutral
-    sevenSeg(1);
-  }*/
-  /*Serial.print("rpm: ");
-  Serial.println(rpmAvg);
-  
-  Serial.println("");*/
-  /*for(int iter = 0;iter<8;iter++) {
-    sevenSeg(iter);
-    //shiftOut(Data7, CP7, MSBFIRST, 1<<iter);
-    delay(500);
-  }*/
-  delay(500);
+
+  //delay(500);
 }
-void setGear() {
-  //delay(10);
+void setGear() { //int 1
+  if( PINB & (1<<(neutral-8))) {          // untested
+    g_selectedGear = 1; //1 being neutral
+    return;
+  }
   bool gearUpState = (PINB & (1<<(gearUp-8)));
   bool gearDownState = (PINB & (1<<(gearDown-8)));
   unsigned long currentInterrupt = micros();
@@ -115,7 +106,7 @@ unsigned int calcRpmAvg() {
   return sum/10;
 }
 
-void blink() {
+void blink() { // int0
   unsigned long currentInterrupt = micros();
   if((currentInterrupt - g_lastInterrupt ) > 5*1000) { //debounce
     calculateRpm(currentInterrupt);
@@ -153,7 +144,7 @@ void sevenSeg(short symbol) {
   
   shiftOut(Data7, CP7, LSBFIRST, ~digitTmp);
   PORTB |= 1; //(1<<Latch7);
-  delayMicroseconds(10); //datasheet segir 200ns min en þarf alveg 5µs t_w
+  delayMicroseconds(10); //datasheet segir 200ns min en þarf alveg 10µs t_w
   //delay(10);
   PORTB &= ~1; //(1<<Latch7);
 }
